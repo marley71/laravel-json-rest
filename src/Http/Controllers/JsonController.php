f28983b504e1cf261a915b89561321caccb7871d
Pier Paolo Ciullo <pierpaolo.ciullo@buongiorno.com>
2019-01-23 15:53:34 +0100

    aggiunto composer e struttura

---
+++ src/Http/Controllers/JsonController.php
@@ -0,0 +1,17 @@
+<?php namespace Marley71\JsonRest\Controllers;
+/**
+ * Created by PhpStorm.
+ * User: pier
+ * Date: 23/01/2019
+ * Time: 15:45
+ */
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Console\Command;
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Facades\Config;
+
+class JsonController extends Controller {
+
+}
